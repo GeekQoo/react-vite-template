766c4a4e0612efc743f1a6abc23f52ff737657db
SniperXu <398622064@qq.com>
2023-09-08 17:39:10 +0800

    :sparkles: feat:新增部分样式配置

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { routes } from "./router";
+import "./styles/global.scss";
 import "uno.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
