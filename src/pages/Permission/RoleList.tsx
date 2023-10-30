b014cd1753d169d2e5da71cb6449532cf66737d7
SniperXu <398622064@qq.com>
2023-10-30 17:49:45 +0800

    :rocket: perf:角色管理列表优化table操作

--- src/pages/Permission/RoleList.tsx
+++ src/pages/Permission/RoleList.tsx
@@ -1,35 +1,76 @@
 import React, { useEffect, useState } from "react";
 import { Card, Table } from "antd";
-import type { ColumnsType } from "antd/es/table";
-import { GET_ROLE_LIST } from "@/api/perimission.ts";
+import { GET_ROLE_LIST } from "@/api/permission.ts";
+import type { ColumnsType, TablePaginationConfig } from "antd/es/table";
 
-interface RowProps {
+interface TableDataProps {
     id: string | number;
     name: string;
 }
 
+interface TableParamProps {
+    pagination?: TablePaginationConfig;
+}
+
 let RoleList: React.FC = () => {
+    let [tableLoading, setTableLoading] = useState<boolean>(false);
+    let [tableData, setTableData] = useState<TableDataProps[]>([]);
+    let [tableParams, setTableParams] = useState<TableParamProps>({
+        pagination: {
+            showQuickJumper: true,
+            showSizeChanger: true,
+            current: 1,
+            pageSize: 10,
+            total: 0
+        }
+    });
+
     useEffect(() => {
         getTableData();
-    }, []);
+    }, [tableParams.pagination?.current, tableParams.pagination?.pageSize]);
 
-    let [tableData, setTableData] = useState<RowProps[]>([]);
-
-    let getTableData = () => {
-        GET_ROLE_LIST<RowProps[]>({}).then((res) => {
-            setTableData(res.data.data ?? []);
-        });
-    };
-
-    let tableColumns: ColumnsType<RowProps> = [
+    let tableColumns: ColumnsType<TableDataProps> = [
         { title: "ID", align: "center", dataIndex: "id" },
         { title: "角色名称", align: "center", dataIndex: "name" }
     ];
 
+    let getTableData = () => {
+        setTableLoading(true);
+        GET_ROLE_LIST<TableDataProps[]>({
+            page: tableParams.pagination?.current,
+            size: tableParams.pagination?.pageSize
+        }).then((res) => {
+            setTableData(res.data.data ?? []);
+            setTableParams({
+                ...tableParams,
+                pagination: {
+                    ...tableParams.pagination,
+                    total: res.data.total ?? 0
+                }
+            });
+            setTableLoading(false);
+        });
+    };
+
+    let handleTableChange = (pagination: TablePaginationConfig) => {
+        setTableParams({ pagination });
+        if (pagination.pageSize !== tableParams.pagination?.pageSize) {
+            setTableData([]);
+        }
+    };
+
     return (
         <div className="role-list-page">
             <Card>
-                <Table bordered dataSource={tableData} columns={tableColumns} />
+                <Table
+                    bordered
+                    dataSource={tableData}
+                    loading={tableLoading}
+                    columns={tableColumns}
+                    pagination={tableParams.pagination}
+                    onChange={handleTableChange}
+                    rowKey={(row) => row.id}
+                />
             </Card>
         </div>
     );
